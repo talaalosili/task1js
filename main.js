f6479b003d40c36b78e32324664cf222cfc62146
talaalosili <talaalosili@gmail.com>
2024-06-27 08:25:59 +0300

    ta

---
+++ main.js
@@ -0,0 +1,180 @@
+// task1
+// let cash = 1000;
+// let ex = 500;
+// let cashFlow = cash / ex;
+// console.log(cashFlow);
+// console.log(cash - ex);
+// console.log(cash + ex);
+// console.log(cash * ex);
+// let a = 7;
+// let b = 9;
+// let c = 2;
+// let sum = a + b + c;
+// console.log(sum / 3);
+// let d = 150 * (30 / 100);
+// console.log(150 - d);
+// let e = 30;
+// let f = 18;
+// let g = 20;
+// console.log(a > f < g);
+// console.log(Math.pow(2, 3));
+// let r = 10 % 4;
+// console.log(r);
+
+//task2
+
+// console.log(typeof(100));
+// console.log(typeof(73.9));
+// console.log(typeof(NaN));
+// console.log(typeof("Water"));
+// console.log(typeof(false));
+// console.log(typeof(9 != 11));
+// console.log("Orang" + "e");
+// console.log("Orange" - "s");
+// console.log("4" + "8");
+// console.log("4" - "8");
+// console.log("name" + 3);
+// console.log("name" - 3);
+// console.log(82 * "word");
+// console.log(1 + "hello");
+// console.log("hello" + 1);
+// console.log(1 + true);
+// console.log("hello" + true);
+// console.log(typeof (Infinity));
+// console.log(1 == '1');
+// console.log(1 === '1');
+
+
+//task3
+
+// let str = "Welcome to Orange";
+// let upperCaseStr = str.toUpperCase();
+// console.log(upperCaseStr); // "WELCOME TO ORANGE"
+
+
+// let subStr = str.slice(8, 10).toUpperCase();
+// console.log(subStr);
+
+
+// let newStr = str.replace("Welcome to", "Hello from");
+// console.log(newStr);
+
+
+// let lowerCaseStr = str.toLowerCase();
+// console.log(lowerCaseStr);
+
+
+// let strLength = str.length;
+// console.log(strLength);
+
+
+// let quotedStr = str.replace("Orange", '"Orange"');
+// console.log(quotedStr);
+
+
+// let appendedStr = str + " Jordan";
+// console.log(appendedStr);
+
+//task4
+
+function myFunction() {
+    alert("Welcome to the website")
+}
+
+//task5
+
+// let str = "another string";
+// let num = 42;
+// let bool = false;
+// let arr = [1.1, 2.2, 3.3];
+// let obj = { key: "value" };
+
+// let str1 = "string value";
+// let num1 = 123;
+// let bool1 = true;
+// let arr1 = [true, "string", 123];
+// let obj1 = { a: 1, b: 2, c: 3 };
+
+// console.log(str, num, bool, arr, obj);
+// console.log(str1, num1, bool1, arr1, obj1);
+
+
+//task6
+
+// if (true) {
+//     var x = 10;
+//     console.log("inside if block (var):", x);
+
+// }
+// console.log("Outside if block (var):", x);
+
+// if (true) {
+//     let y = 10;
+//     console.log("inside if block (let):", y);
+// }
+// console.log("Outside if block (let):", typeof y);
+
+//task7 1
+
+// let a = 10;
+// let b = 5;
+// let add = a + b;
+// let sub = a - b;
+// let multi = a * b;
+// let div = a / b;
+// let mod = a % b;
+
+// console.log(add);
+// console.log(sub);
+// console.log(multi);
+// console.log(div);
+// console.log(mod);
+
+
+//task7 2
+
+// let a = 10;
+// let b = 5;
+// Greater = a > b;
+// Less = a < b;
+// Equal = a == b;
+// notequ = a != b;
+// greaterequal = a >= b;
+// lessequal = a <= b;
+
+
+// console.log(Greater)
+// console.log(Less)
+// console.log(Equal)
+// console.log(notequ)
+// console.log(greaterequal)
+// console.log(lessequal)
+
+
+
+//task7 3
+// let x = true;
+// let y = false;
+// AND = x && y;
+// OR = x || y;
+// NOTx = !x;
+// NOTy = !y;
+// console.log(AND)
+// console.log(OR)
+// console.log(NOTx)
+// console.log(NOTy)
+
+
+//task7 4
+// let a = 10;
+// Add = a += 5
+// Subtract = a -= 5
+// Multiply = a *= 2
+// console.log(Add)
+// console.log(Subtract)
+// console.log(Multiply)
+
+//task8
+// document.write("<p>Message using document.write()</p>");
+// document.getElementById("message").innerHTML = "<p>Message using innerHTML</p>";
+// console.log("Message logged to console");
